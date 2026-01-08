d2a3ef577ca67b8d861720e2f7645f29170365ac
Zhang Lu <1525347915@qq.com>
2026-01-08 21:02:08 +0800

    Add files via upload

---
+++ bash_scripts/estimate_k.sh
@@ -0,0 +1,13 @@
+PYTHON='/home/zhanglu/.conda/envs/gcd/bin/python'
+
+#hostname
+
+# Get unique log file
+SAVE_DIR=/home/zhanglu/GCD/generalized-category-discovery-main/osr_categories/dev_outputs/
+
+EXP_NUM=$(ls ${SAVE_DIR} | wc -l)
+EXP_NUM=$((${EXP_NUM}+1))
+echo $EXP_NUM
+
+${PYTHON} -m methods.estimate_k.estimate_k --max_classes 1000 --dataset_name herbarium_19 --search_mode other \
+        > ${SAVE_DIR}logfile_${EXP_NUM}.out
